822d406297b46f02f9a6c1d02c91a158b9e356a8
Trinity79 <miglanichetanya79@gmail.com>
2023-12-04 14:55:42 +0530

    server change2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     let filePath = '.' + req.url;
     if (filePath === './') {
-        filePath = './index1.html';
+        filePath = './index.html';
     }
 
     const extname = path.extname(filePath);
